d0f4f3a628e13b506744da36cf41e81ad74b6456
chenliqiong <312676764@qq.com>
2021-01-16 21:02:44 +0800

    network

---
+++ models/imagenet/resnet_sksa.py
@@ -0,0 +1,302 @@
+#encoding:utf-8
+''''
+date: 20191219
+author: clq
+description: (1) modified based on resnet_sk.py and resnet_cbam.py
+                      (2)  add spatial attention in SKNet
+'''
+
+from .common_head import *
+
+__all__=['sksa_resnet18', 'sksa_resnet34', 'sksa_resnet50', 'sksa_resnet101', 'sksa_resnet152']
+
+model_urls = {
+    'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
+    'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
+    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
+    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
+}
+###########################################################
+class BasicConv(nn.Module):
+    def __init__(self, in_planes, out_planes, kernel_size, stride=1, padding=0 \
+            , dilation=1, groups=1, relu=True, bn=True, bias=False):
+        super(BasicConv, self).__init__()
+        self.out_channels = out_planes
+        self.conv = nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size \
+                , stride=stride, padding=padding, dilation=dilation \
+                , groups=groups, bias=bias)
+        self.bn = nn.BatchNorm2d(out_planes,eps=1e-5, momentum=0.01 \
+                , affine=True) if bn else None
+        self.relu = nn.ReLU() if relu else None
+
+    def forward(self, x):
+        x = self.conv(x)
+        if self.bn is not None:
+            x = self.bn(x)
+        if self.relu is not None:
+            x = self.relu(x)
+        return x
+
+
+# feature map: 4-d tensor, batch*channels*H*W
+class ChannelPool(nn.Module):
+    def forward(self, x):
+        return torch.cat( (torch.max(x,1)[0].unsqueeze(1), torch.mean(x,1) \
+                .unsqueeze(1)), dim=1 )       # dim=1 means along the channel-dim.
+        # torch.cat()    Concatenates the given sequence of seq tensors in the given dimension.
+
+# spatial attention
+class SpatialGate(nn.Module):
+    def __init__(self):
+        super(SpatialGate, self).__init__()
+        kernel_size = 7
+        self.compress = ChannelPool()
+        self.spatial = BasicConv(2, 1, kernel_size, stride=1 \
+                , padding=(kernel_size-1) // 2, relu=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x_compress = self.compress(x)
+        x_out = self.spatial(x_compress)
+        scale = self.sigmoid(x_out) # broadcasting
+        return x * scale
+###########################################################
+
+
+def conv3x3(in_planes, out_planes, stride = 1, groups=1):
+    ''''3x3 convolution with padding'''
+    return nn.Conv2d(in_planes,  out_planes,  kernel_size=3, stride=stride,
+                     padding=1, bias=False, groups=groups)
+
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+        self.SpatialGate = SpatialGate()  # added by CLQ for sksa_resnet
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.SpatialGate(out)  # added by CLQ for sksa_resnet
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = conv1x1(inplanes, planes)
+        self.bn1 = nn.BatchNorm2d(planes)
+
+        self.conv2 = conv3x3(planes, planes, stride)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv2g = conv3x3(planes, planes, stride, groups = 32)
+        self.bn2g   = nn.BatchNorm2d(planes)
+
+        self.conv3 = conv1x1(planes, planes * self.expansion)
+        self.bn3 = nn.BatchNorm2d(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)     # output_size = 1
+        self.conv_fc1 = nn.Conv2d(planes, planes//16, 1, bias=False)
+        self.bn_fc1   = nn.BatchNorm2d(planes//16)
+        self.conv_fc2 = nn.Conv2d(planes//16, 2 * planes, 1, bias=False)
+
+        self.SpatialGate = SpatialGate()     # added by CLQ for sksa_resnet
+
+        self.D = planes
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        d1 = self.conv2(out)
+        d1 = self.bn2(d1)
+        d1 = self.relu(d1)
+
+        d2 = self.conv2g(out)
+        d2 = self.bn2g(d2)
+        d2 = self.relu(d2)
+
+        d  = self.avg_pool(d1) + self.avg_pool(d2)
+        d = F.relu(self.bn_fc1(self.conv_fc1(d)))
+        d = self.conv_fc2(d)
+        d = torch.unsqueeze(d, 1).view(-1, 2, self.D, 1, 1)      # expand the second dim, -1 means keep the original size.
+        d = F.softmax(d, 1)     # dim = 1 ,  which means the dimension along which softmax will be computed.
+        d1 = d1 * d[:, 0, :, :, :].squeeze(1)
+        d2 = d2 * d[:, 1, :, :, :].squeeze(1)
+        d  = d1 + d2
+
+        out = self.conv3(d)
+        out = self.bn3(out)
+        out = self.SpatialGate(out)       # added by CLQ for sksa_resnet
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, layers, num_classes=1000, zero_init_residual=False):
+        super(ResNet, self).__init__()
+        self.inplanes = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    nn.init.constant_(m.bn3.weight, 0)
+                elif isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+
+#####################################################
+def sksa_resnet18(pretrained = False, **kwargs):
+    ''''Constructs a ResNet-18 model.
+    Args:
+             pretrained (bool) : If True, returns a model pre-trained on ImageNet
+    '''
+    model = ResNet(BasicBlock, [2,2,2,2], **kwargs)
+    return model
+
+
+def sksa_resnet34(pretrained=False, **kwargs):
+    """Constructs a ResNet-34 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(BasicBlock, [3, 4, 6, 3], **kwargs)
+    return model
+
+
+def sksa_resnet50(pretrained=False, **kwargs):
+    """Constructs a ResNet-50 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
+    return model
+
+
+def sksa_resnet101(pretrained=False, **kwargs):
+    """Constructs a ResNet-101 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(Bottleneck, [3, 4, 23, 3], **kwargs)
+    return model
+
+
+def sksa_resnet152(pretrained=False, **kwargs):
+    """Constructs a ResNet-152 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(Bottleneck, [3, 8, 36, 3], **kwargs)
+    return model
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
